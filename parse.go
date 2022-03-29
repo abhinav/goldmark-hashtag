16b93afbb2aa2fa93d82ddbc78cbed204a25db42
Abhinav Gupta <mail@abhinavg.net>
2022-03-28 19:04:00 -0700

    variants: Document accepted characters
    Explain what's accepted in the default and obsidian variants.

--- parse.go
+++ parse.go
@@ -14,10 +14,9 @@ import (
 
 // Parser is a Goldmark inline parser for parsing hashtag nodes.
 //
-// Hashtags start with # and an alphabet, followed by zero or more
-// alphanumeric characters and the following symbols.
-//
-//  /_-
+// Hashtags start with "#". The list of other characters allowed in the hashtag
+// is determined by variant. See the documentation for Variant for more
+// details.
 type Parser struct {
 	ParserConfig
 }
@@ -32,8 +31,28 @@ type ParserConfig struct {
 type Variant uint
 
 const (
-	DefaultVariant  Variant = 0
-	ObsidianVariant         = iota
+	// DefaultVariant is the default flavor of hashtag syntax supported by
+	// this package.
+	//
+	// In this format, hashtags start with "#" and an alphabet, followed by
+	// zero or more alphanumeric characters and the following symbols.
+	//
+	//   /_-
+	DefaultVariant Variant = 0
+
+	// ObsidianVariant is a flavor of the hashtag syntax that aims to be
+	// compatible with Obsidian (https://obsidian.md/).
+	//
+	// In this format, hashtags start with "#" followed by alphabets,
+	// numbers, emoji, or any of the following symbols.
+	//
+	//   /_-
+	//
+	// Hashtags cannot be entirely numeric and must contain at least one
+	// non-numeric character.
+	//
+	// See also https://help.obsidian.md/How+to/Working+with+tags.
+	ObsidianVariant = iota
 )
 
 var _ parser.InlineParser = (*Parser)(nil)
