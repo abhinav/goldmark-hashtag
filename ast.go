7aebd280029aa3d416799a92e8fe5e76903fca12
Abhinav Gupta <mail@abhinavg.net>
2023-02-11 15:59:32 -0800

    Convert module into a shim around new path
    Turn the v0.2 release into a shim around v0.3
    which uses a different import path.

--- ast.go
+++ ast.go
@@ -1,24 +1,9 @@
 package hashtag
 
-import "github.com/yuin/goldmark/ast"
+import "go.abhg.dev/goldmark/hashtag"
 
 // Kind is the kind of hashtag AST nodes.
-var Kind = ast.NewNodeKind("Hashtag")
+var Kind = hashtag.Kind
 
 // Node is a hashtag node in a Goldmark Markdown document.
-type Node struct {
-	ast.BaseInline
-
-	// Tag is the portion of the hashtag following the '#'.
-	Tag []byte
-}
-
-// Kind reports the kind of hashtag nodes.
-func (*Node) Kind() ast.NodeKind { return Kind }
-
-// Dump dumps the contents of Node to stdout for debugging.
-func (n *Node) Dump(src []byte, level int) {
-	ast.DumpHelper(n, src, level, map[string]string{
-		"Tag": string(n.Tag),
-	}, nil)
-}
+type Node = hashtag.Node
